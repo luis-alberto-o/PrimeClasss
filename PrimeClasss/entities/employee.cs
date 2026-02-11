8612c8eed5a79c340205f3ba8d75e7bbeecd9864
luisa <luisa@MSI>
2026-02-10 20:25:14 -0400

    Agregar archivos de proyecto.

---
+++ PrimeClasss/entities/employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PrimeClasss.entities;
+
+namespace PrimeClasss.entities.employees
+{
+    public class Employee : Community
+    {
+        public double Salary { get; set; }
+
+        public void Work()
+        {
+            Console.WriteLine($"{Name} is working");
+        }
+    }
+}
+
---
+++ PrimeClasss/entities/students.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeClasss.entities
+{
+    public class Students : Community
+    {
+        public string Career { get; set; }
+
+        public void Study()
+        {
+            Console.WriteLine($"{Name} is studying {Career}");
+        }
+    }
+}
+
